bda452f06b2f3f577bdff0a62bd71f9952634c99
KumarGangesh123 <116815600+KumarGangesh123@users.noreply.github.com>
2022-11-16 00:03:33 -0800

    Update linkedListSorting.java

--- linkedListSorting.java
+++ linkedListSorting.java
@@ -1,1 +1,24 @@
 /* this code is use to sort the linked list */
+
+/*
+  in it a package named "dsa" included 
+  first of all you have to keep the files "Node.java" and "linkedListSorting.java" together in a folder then :
+  for complie this file write on terminal -> javac -d . linkedListSorting.java
+  then run the file by -> java dsa.linkedListSorting
+*/
+
+
+package dsa;
+import java.util.Scanner;
+public class linkedListSorting {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the range :\t");
+        int range=sc.nextInt();
+        Node nd=new Node();
+        Node.node head=nd.connectNodes(range);
+        nd.displayNodes(head);
+        nd.listSorting(head);
+        nd.displayNodes(head);
+    }
+}
