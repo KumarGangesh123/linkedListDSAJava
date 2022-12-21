e34c707392ec4d4776ab663df3c8b4459a600306
KumarGangesh123 <116815600+KumarGangesh123@users.noreply.github.com>
2022-12-21 16:07:14 +0530

    Update linkedListInsertion.java

--- linkedListInsertion.java
+++ linkedListInsertion.java
@@ -9,7 +9,75 @@
 
 
 package dsa;
-import java.util.Scanner;
+class Node {
+    class node{     // this is the node class to create the node of a linked list
+        int data;
+        node next;
+        public node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+
+    public node createNode(int data){     // for creating a new node of the linked list
+        return new node(data);
+    }
+
+    public node connectNodes(int range){      // this method for connecting the nodes and return the head pointer of the list
+        Scanner sc=new Scanner(System.in);
+        System.out.print("\nEnter the data for the 1 node :\t");
+        int data1=sc.nextInt();
+        node newNode = createNode(data1);
+        node n1=newNode;
+        node n2=null;
+        node head=n1;
+        for(int x=2;x<=range;x++){
+            System.out.print("\nEnter the data for the "+x+" node :\t");
+            int data2=sc.nextInt();
+            n2=createNode(data2);
+            n1.next=n2;
+            n1=n1.next;
+        }
+        return head;
+    }
+  
+   public node insertAtPos(node head,int pos,int data){      // this method will return the node , in this the new data will be inserted on the specified position
+        node n1=head;
+        node n2=head;
+        int x=1;
+        while(x != pos-1){
+            n2=n2.next;
+            x++;
+        }
+        node newNode = createNode(data);
+        newNode.next=n2.next;
+        n2.next=newNode;
+        return n1;
+    }
+
+    public node insertAtLast(node head,int data){       // this method will return the node , in which the new data will be added at the last of the list
+        node n1=head;
+        node n2=head;
+        node newNode = createNode(data);
+        while(n2.next != null){
+            n2=n2.next;
+        }
+        n2.next=newNode;
+        return n1;
+    }
+  
+   public void displayNodes(node head){      // this method is use to display the linked list
+        int x=0;
+        System.out.print("\n\nData in the linked list is :\n\n");
+        while(head != null){
+            System.out.print("\nData in the "+(x+1)+" node is :\t"+head.data);
+            head=head.next;
+            x++;
+        }
+    }
+}
+
+
 public class linkedListInsertion {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
